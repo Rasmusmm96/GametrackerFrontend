5c3ccafa14d8c2b9d334ecf6f5978a8ecb53705c
Rasmusmm96 <rallemm96@gmail.com>
2018-02-20 21:16:09 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import {GameService} from './services/game.service';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {RouterModule, Routes} from '@angular/router';
+import { AddGameComponent } from './add-game/add-game.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'addgame', component: AddGameComponent }
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ToolbarComponent,
+    AddGameComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [
     GameService
